35d71758ec9cf6c833664bde2beea02180bb2ff7
nandinigandhi21 <nandinigandhi215@gmail.com>
2025-08-30 21:54:43 +0530

    Gitignore Commit

--- backend/app.py
+++ backend/app.py
@@ -3,12 +3,16 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 import random, smtplib
 from email.mime.text import MIMEText
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
 
 # ---------------- Database ---------------- #
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///eventhive.db'
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
